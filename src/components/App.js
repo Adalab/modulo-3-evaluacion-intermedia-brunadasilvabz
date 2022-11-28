be473a678e19f97421dff77d5e20417758541e81
Bruna <brunadasilvabz98@gmail.com>
2022-11-28 09:24:31 +0100

    add api + basic html structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 import "../styles/App.scss";
+import adalabers from "../data/adalabers.json";
+import { useState } from "react";
 
 function App() {
+  console.log(adalabers);
+  const [data, setData] = useState(adalabers);
+
   return (
     <div className="App">
-      <h1 className="title">Hooolis </h1>Relléname!
-      <form action="" className="form">
-        <label htmlFor="">El nombre:</label>
-        <input className="input" type="text" placeholder="Holis" maxLength="" />
-      </form>
+      <h1>Adalabers</h1>
+      <table border="1" className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>MariCarmen</td>
+            <td>Yanelis</td>
+            <td>Python</td>
+          </tr>
+
+          <tr>
+            <td>Amparo</td>
+            <td>Dayana</td>
+            <td>IA</td>
+          </tr>
+
+          <tr>
+            <td>Escandia</td>
+            <td>Iván</td>
+            <td>3D graphics</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
