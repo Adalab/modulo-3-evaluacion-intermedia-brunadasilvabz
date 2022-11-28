819690ddbbd65ec13471c46e92ff3b1404e6aa35
Bruna <brunadasilvabz98@gmail.com>
2022-11-28 08:42:42 +0100

    add react kit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Hooolis </h1>Relléname!
+      <form action="" className="form">
+        <label htmlFor="">El nombre:</label>
+        <input className="input" type="text" placeholder="Holis" maxLength="" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
